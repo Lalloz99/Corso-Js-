49b5ae27eadad932b3e9c56022e6cec57aac58a9
Lalloz99 <100274174+Lalloz99@users.noreply.github.com>
2022-02-23 17:59:01 +0100

    Primo commit

---
+++ 23_02/esercizio.js
@@ -0,0 +1,37 @@
+//function restituisci(ciao){
+    /*for(var i=0; i<ciao.length; i++){
+        if(ciao[i]=="0")
+        continue;
+        if(ciao[i]=="1")
+        continue;
+        if(ciao[i]=="2")
+        continue;
+        if(ciao[i]=="3")
+        continue;
+        if(ciao[i]=="4")
+        continue;
+        if(ciao[i]=="5")
+        continue;
+        if(ciao[i]=="6")
+        continue;
+        if(ciao[i]=="7")
+        continue;
+        if(ciao[i]=="8")
+        continue;
+        if(ciao[i]=="9")
+        continue;*/
+        
+
+    
+
+
+
+//} 
+function sostituzione(x,n){
+    if(n>x.length) {console.log("il numero")
+    return x;
+}
+var y=""
+for (let i=0;i<n;)
+    }
+}
